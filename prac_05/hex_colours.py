fded403dcacfb635222704283c4843dbd85d5e20
AshtonStewart <ashton.stewart@my.jcu.edu.au>
2024-10-19 16:51:39 +1000

    created colour dictionary

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,14 @@
+"""
+CP1404 - Ashton Stewart
+hex colours in a dictionary
+"""
+
+hex_colour_codes = {"Absolute Zero": "#0048ba",
+                    "Aqua": "#00ffff",
+                    "Baby Blue": "#89cff0",
+                    "Bisque1": "#ffe4c4",
+                    "Bleu de France": "#318ce7",
+                    "Bright Green": "#66ff00",
+                    "Chestnut": "#954535",
+                    "Cinereous": "#98817b",
+                    "CornflowerBlue": "#6495ed"}
