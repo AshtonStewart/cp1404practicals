eb00f27a52af4c4053c2cd9181a4e0b9d6bd829f
AshtonStewart <ashton.stewart@my.jcu.edu.au>
2024-10-20 16:45:44 +1000

    started_code

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -9,20 +9,36 @@ thing = []
 
 import csv
 #Champion and how many times they've won
+def main():
+    with open('wimbledon.csv', "r", encoding="utf-8-sig") as in_file:
+        reader = csv.reader(in_file)
 
-with open('wimbledon.csv', "r", encoding="utf-8-sig") as in_file:
-    reader = csv.reader(in_file)
-    first_row_excluder = 0
-    for row in reader:
-        if first_row_excluder != 0:
-            champions_and_wins_total[row[2]] = row[5]
-            thing = row[5]
-        first_row_excluder += 1
-    print(thing)
+        c_and_ws = champion_scorer()
+        first_row_excluder = 0
+        for row in reader:
+            if first_row_excluder != 0:
+                champions_and_wins_total[row[2]] = row[5]
 
 
-for champion, total_wins in champions_and_wins_total.items():
-    print(f"{champion}: {total_wins}")
+            first_row_excluder += 1
+    #print(thing)
+
+def country_name_sorter():
+    print("bok")
+
+def champion_scorer():
+    #score = row[5].split(":")[0]
+    #print(f"bok {score}")
+    print("bok")
+
+def info_printer():
+    print("bok")
+
+#need to finish
+
+
+#for champion, total_wins in champions_and_wins_total.items():
+   #print(f"{champion}: {total_wins}")
 
 
 #print(champions_and_wins_total)
