3d32e9b8c4202744d92f28f87d298bc2648ac3d8
AshtonStewart <ashton.stewart@my.jcu.edu.au>
2024-10-20 21:23:49 +1000

    added comments

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -4,42 +4,50 @@ Processing csv files using nests and dictionaries
 """
 
 
-champions_and_wins_total = {}
-thing = []
 
 import csv
-#Champion and how many times they've won
+
+
 def main():
+    champions_and_wins_total, winning_countries = get_data()
+    champion_data_printer(champions_and_wins_total)
+    country_data_printer(winning_countries)
+
+def get_data():
+    #creates empty dictionary
+    champions_and_wins_total = {}
+    #creates empty list
+    winning_countries = []
+    #opens and reads the csv file
     with open('wimbledon.csv', "r", encoding="utf-8-sig") as in_file:
         reader = csv.reader(in_file)
-
-        c_and_ws = champion_scorer()
-        first_row_excluder = 0
-        for row in reader:
-            if first_row_excluder != 0:
-                champions_and_wins_total[row[2]] = row[5]
-
-
-            first_row_excluder += 1
-    #print(thing)
-
-def country_name_sorter():
-    print("bok")
-
-def champion_scorer():
-    #score = row[5].split(":")[0]
-    #print(f"bok {score}")
-    print("bok")
-
-def info_printer():
-    print("bok")
-
-#need to finish
+        next(reader)
+        #loops through each line and gets relevant information and stores in a list
+        for each_line in reader:
+            country = each_line[1]
+            champion_name = each_line[2]
+            #adds countries if not in the list
+            if country not in winning_countries:
+                winning_countries.append(country)
+            if champion_name in list(champions_and_wins_total.keys()):
+                champions_and_wins_total[champion_name] += 1
+            else:
+                champions_and_wins_total[champion_name] = 1
+    #returns stuff for the main function to use in other functions
+    return champions_and_wins_total, winning_countries
 
 
-#for champion, total_wins in champions_and_wins_total.items():
-   #print(f"{champion}: {total_wins}")
+def country_data_printer(country_data):
+    print("The 12 winning countries are:")
+    #sorts data alphabetically:
+    country_data.sort()
+    #prints all contents in a row at once:
+    print(*country_data)
 
+def champion_data_printer(win_data):
+    print("Champions:")
+    #prints every champion name (key) and their wins(value) from the dictionary
+    for champion_name, wins in win_data.items():
+        print(f"{champion_name} won {wins} times!")
 
-#print(champions_and_wins_total)
-#counties of the champions in alphabetical order
+main()
