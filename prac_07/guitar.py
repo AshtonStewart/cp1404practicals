97fdf5525e4ce8016bfe7ebad34365b8ab3743af
AshtonStewart <ashton.stewart@my.jcu.edu.au>
2024-11-08 15:49:17 +1000

    Created initials and copied guitar over

---
+++ prac_07/guitar.py
@@ -0,0 +1,30 @@
+"""
+Ashton Jack Stewart
+Practical 07 - guitar
+"""
+
+class Guitar:
+    def __init__(self, name = "", year = 0, cost = 0):
+        """Initialist a guitar instance.
+            name = text
+            year = int
+            cost = float (1:1) for dollars"""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+
+        """prints details of the guitar"""
+        return f"{self.name} ({self.year}) : ${self.cost}"
+
+    def get_age(self):
+        """gets the age of the guitar"""
+        current_year = 2024
+        return_year = current_year - self.year
+        return return_year
+
+    def is_vintage(self):
+        """Returns True if the guitar is over 50 years old"""
+        vintage_age = 50
+        return self.get_age() >= vintage_age
---
+++ prac_07/myguitars.py
@@ -0,0 +1,4 @@
+"""
+CP1404 - practical 7
+Ashton Jack Stewart
+"""
