509a214c373dac234e22f6e3d4f1d3ff5b5a2e6d
AshtonStewart <ashton.stewart@my.jcu.edu.au>
2024-11-10 21:46:13 +1000

    added update feature

--- prac_07/project.py
+++ prac_07/project.py
@@ -17,4 +17,10 @@ class Project:
         self.C_P = C_P
 
     def __str__(self):
-        return f'{self.name}, start: {self.S_D_P}, priority: {self.priority}, estimate: {self.C_E}, '
+        return f'{self.name}, start: {self.S_D_P}, priority: {self.priority}, estimate: ${self.C_E}'
+
+    def is_completed(self):
+        return self.C_P >= 100
+
+    def to_line(self):
+        return f"{self.name}\t{self.S_D_P}\t{self.priority}\t{self.C_E}\t{self.C_P}"
--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -23,31 +23,32 @@ def main():
     all_projects = []
     chosen_action = "Not nothing"
     while chosen_action != "Q":
-        print(menu)
+
+        print(f"\n {menu}")
         chosen_action = input(">> ").upper()
 
         if chosen_action == "L":
             all_projects = info_get()
 
         elif chosen_action == "S":
-            print("save")
+            save(all_projects)
 
         elif chosen_action == "D":
-            print("Display")
             display(all_projects)
 
         elif chosen_action == "F":
             print("Filter by date")
 
         elif chosen_action == "A":
-            print("Add new project")
             all_projects = add(all_projects)
 
         elif chosen_action == "U":
             print("Update")
+            all_projects = update(all_projects)
 
         elif chosen_action != "Q":
             print("Invalid menu choice")
+    print("Process concluded")
 
 def info_get():
     all_contents = []
@@ -63,23 +64,38 @@ def info_get():
                 line = line.strip()
                 content = line.split("\t")
 
-                #print(content)
-                all_contents.append(content)
+                new_content = Project(name=content[0], S_D_P=content[1], priority=float(content[2]), C_E=float(content[3]), C_P=float(content[4])
+                )
+                all_contents.append(new_content)
             first_line += 1
 
     return all_contents
 
-def save():
+def save(all_projects):
     print("save")
+    with open(file_name, "w") as out_file:
+
+        for new_line in all_projects:
+            out_file.write(new_line.to_line() + '\n')
+
+
 
 def display(info_to_display):
 
-    for line in info_to_display:
-        #def __init__(self, name, S_D_P, C_E, priority, C_P):
-        print(line[0])
+    incomplete_projects = [item for item in info_to_display if not item.is_completed()]
+    complete_projects = [item for item in info_to_display if item.is_completed()]
 
-        #print(Project(line[0].name, info_to_display[1].S_D_P, info_to_display[2].C_E, info_to_display[3].priority, info_to_display[4].C_P))
-        #print(line)
+
+    print("\n\nIncomplete projects: ")
+
+    for each_project in incomplete_projects:
+        print(each_project)
+
+    print("\n\nComplete projects: ")
+    for each_project in complete_projects:
+        print(each_project)
+
+
 
 def add(current_contents):
     new_project_info = []
@@ -88,45 +104,41 @@ def add(current_contents):
     #print(f"That day is/was {date.strftime('%A')}")
     #print(date.strftime("%d/%m/%Y"))
 
-    #new_project_info.append((
     new_project_info.append(input("New project name: "))
-    new_project_info.append(date_string)
+    new_project_info.append(date)
     new_project_info.append(float(input("What number priority is it: ")))
     new_project_info.append(float(input("What is the expected cost: ")))
     new_project_info.append(float(input("What is the percentage completion (do not type '%'): ")))
 
-    current_contents.append(Project(new_project_info))
+    current_contents.append(Project(new_project_info[0], new_project_info[1], new_project_info[2], new_project_info[3], new_project_info[4]))
 
-    print(current_contents)
     return current_contents
 
+
+def update(current_contents):
+
+    matches_name = False
+
+
+    while matches_name == False:
+        project_to_update = input("Please type the name of the project you'd like to update: ")
+        #print(current_contents)
+        for project in current_contents:
+            if project.name == project_to_update:
+                matches_name = True
+
+    new_C_P = float(input(f"What is the new completion % of {project_to_update} (do not add '%'): "))
+
+    for project in current_contents:
+        if project.name == project_to_update:
+            project.C_P = new_C_P
+
+
+    return current_contents
+
+
+
+
+
+
 main()
-
-
-#  print(info_to_display)
-#     for line in info_to_display:
-#         print(line.name)
-#
-# def add(current_contents):
-#     new_project_info = []
-#     date_string = input("Date (d/m/yyyy): ")  # e.g., "30/9/2022"
-#     date = datetime.datetime.strptime(date_string, "%d/%m/%Y").date()
-#     #print(f"That day is/was {date.strftime('%A')}")
-#     #print(date.strftime("%d/%m/%Y"))
-#     S_D_P = date
-#     #new_project_info.append((
-#     name = input("New project name: ")
-#
-#     #new_project_info.append(date_string)
-#
-#     C_E = float(input("What is the expected cost: "))
-#     priority = float(input("What number priority is it: "))
-#     C_P = float(input("What is the percentage completion (do not type '%'): "))
-#
-#     current_contents.append(Project(name, S_D_P, C_E, priority, C_P))
-#
-#     print(current_contents)
-#     return current_contents
-#
-# main()
-# #print(Project(name, S_D_P, C_E, priority, C_P))
