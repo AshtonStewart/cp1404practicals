c9d9a65f4e195935913f4eebaf34820e30c86317
AshtonStewart <ashton.stewart@my.jcu.edu.au>
2024-11-18 14:50:19 +1000

    Added docstrings

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -1,8 +1,14 @@
+"""
+CP1404 - Practical 08: Box Layout Demo
+Ashton Stewart
+"""
+
 from kivy.app import App
 from kivy.lang import Builder
 
 
 class BoxLayoutDemo(App):
+    """Builds GUI"""
     def build(self):
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
@@ -13,6 +19,7 @@ class BoxLayoutDemo(App):
         self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
 
     def handle_clear(self):
+        """Clears all text from the output_label box"""
         self.root.ids.output_label.text = ""
 
 
