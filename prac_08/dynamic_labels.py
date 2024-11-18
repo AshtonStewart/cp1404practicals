3ae280b1b403db643f7f7cc95f2e135d44cf4681
AshtonStewart <ashton.stewart@my.jcu.edu.au>
2024-11-18 20:23:46 +1000

    Button creates new button when pressed

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -6,12 +6,26 @@ Ashton Stewart
 
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.button import Button
+from kivy.properties import StringProperty
 
 class DynamicLayoutApp(App):
+
+    def __init__(self, **kwargs):
+        '''Defines a generic bunch of names'''
+        super().__init__(**kwargs)
+        self.names = ["Ashton, Bok, Bringus, Brongus, Bingus, Bongus"]
+
     def build(self):
+        '''Builds the UI'''
         self.title = "Dynamic demo"
         self.root = Builder.load_file("dynamic_labels.kv")
-        print("test")
+        self.new_widgets()
         return self.root
+
+    def new_widgets(self):
+        print("Begone empty function warning")
+        for name in self.names:
+            return self.root.add_widget(Button(text=name))
 
 DynamicLayoutApp().run()
