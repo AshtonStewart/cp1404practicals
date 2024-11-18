901414720acf5489ff843ea9610edc01923dc067
AshtonStewart <ashton.stewart@my.jcu.edu.au>
2024-11-18 15:15:46 +1000

    Edited error checking output and added constant for converting units

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -7,22 +7,24 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
 
+multiplier = 1.60934
 
 class DistanceConverterLayout(App):
     def build(self):
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('convert_miles_km.kv')
+        #Window.size = (750, 500)
         return self.root
 
     def convert_distance(self, value):
         """Converts the user input into an output"""
         try:
             distance = float(value)
-            distance *= 1.6
+            distance *= multiplier
             self.root.ids.output_distance.text = str(distance)
 
         except ValueError:
-            self.root.ids.output_distance.text = "Invalid Distance"
+            self.root.ids.output_distance.text = str(0)
 
     def input_change(self, value, action):
         """Changes input value based on the pressed button"""
@@ -36,6 +38,6 @@ class DistanceConverterLayout(App):
             self.root.ids.input_number.text = str(distance)
 
         except ValueError:
-            self.root.ids.output_distance.text = "Invalid Distance"
+            self.root.ids.output_distance.text = str(0)
 
 DistanceConverterLayout().run()
