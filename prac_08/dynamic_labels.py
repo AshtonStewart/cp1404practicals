909f0bfbe1e849d56c949370eaf0f024c9d78e51
AshtonStewart <ashton.stewart@my.jcu.edu.au>
2024-11-18 19:46:15 +1000

    Initial commit

---
+++ prac_08/dynamic_labels.py
@@ -0,0 +1,14 @@
+"""
+CP1404 - Practical 08
+Dyanmic Labels
+Ashton Stewart
+"""
+
+from kivy.app import App
+from kivy.lang import Builder
+
+class DynamicLayoutApp(App):
+    def build(self):
+        print("test")
+
+DynamicLayoutApp.run()
