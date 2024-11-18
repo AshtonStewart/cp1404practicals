4744b07044d692f2fb6303437b497cee566f7fde
AshtonStewart <ashton.stewart@my.jcu.edu.au>
2024-11-18 14:11:41 +1000

    Initial commit / setup

---
+++ prac_08/convert_miles_km.py
@@ -0,0 +1,13 @@
+"""
+CP1404 - Practical 08: Convert Miles to Km
+Ashton Stewart
+"""
+
+
+from kivy.app import App
+from kivy.lang import Builder
+
+
+#class DistanceConverterLayout(App):
+
+#BoxLayoutDemo().run()
