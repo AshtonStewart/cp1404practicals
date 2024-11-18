bb9b1830506998489fe3e53c17841e4af464db9c
AshtonStewart <ashton.stewart@my.jcu.edu.au>
2024-11-18 19:53:45 +1000

    Got gui to connect to class and display

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -9,6 +9,9 @@ from kivy.lang import Builder
 
 class DynamicLayoutApp(App):
     def build(self):
+        self.title = "Dynamic demo"
+        self.root = Builder.load_file("dynamic_labels.kv")
         print("test")
+        return self.root
 
-DynamicLayoutApp.run()
+DynamicLayoutApp().run()
