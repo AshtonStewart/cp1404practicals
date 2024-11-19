60c9b1c289814300ab42233b7ce831c7224ceb37
AshtonStewart <ashton.stewart@my.jcu.edu.au>
2024-11-19 20:53:16 +1000

    AUtomatically prints all widgets and removed interactivity

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -16,17 +16,17 @@ class DynamicLayoutApp(App):
     def __init__(self, **kwargs):
         '''Defines a generic bunch of names'''
         super().__init__(**kwargs)
-        self.names = ["Ashton, Bok, Bringus, Brongus, Bingus, Bongus"]
+        self.names = ["Ashton", "Bok", "Bringus", "Brongus", "Bingus", "Bongus", "Cam"]
 
     def build(self):
         '''Builds the UI'''
         self.title = "Dynamic demo"
         self.root = Builder.load_file("dynamic_labels.kv")
-        #self.new_widgets()
+        self.new_widgets()
         return self.root
 
     def new_widgets(self):
         for name in self.names:
-            return self.root.add_widget(Label(text=name))
+            self.root.add_widget(Label(text=name))
 
 DynamicLayoutApp().run()
