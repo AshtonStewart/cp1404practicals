61530b11d0ab8d0410ed454972c457f290f4b1e6
AshtonStewart <ashton.stewart@my.jcu.edu.au>
2024-11-23 12:44:19 +1000

    Added fancy multiplier to fare and flagfall

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -10,10 +10,26 @@ from taxi import Taxi
 class silverservicetaxi(Taxi):
     """Represents a fancy taxi object"""
 
-    def __init__(self, name, fuel):
+    def __init__(self, name, fuel, fanciness):
         super().__init__(name, fuel)
-        self.fancy_fare_multi = 2.5
+        self.fancy_fare_multi = fanciness
+        self.flagfall = 4.50
 
 
     def __str__(self):
-        return f"{super().__str__()}"
+        return f"{super().__str__()} Plus flagfall of ${self.flagfall}"
+
+    def get_fare(self):
+        """Return the price for the taxi trip."""
+        return (self.price_per_km * self.current_fare * self.fancy_fare_multi) + self.flagfall
+
+    def start_fare(self):
+        """Begin a new fare."""
+        self.current_fare = 0
+
+    def drive(self, distance):
+        """Drive like parent Car but calculate fare distance as well."""
+        distance_driven = distance
+        self.current_fare += distance_driven
+        return distance_driven
+
