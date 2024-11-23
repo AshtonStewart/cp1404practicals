515a584f03059900325f0b8e4ecadd0e76867759
AshtonStewart <ashton.stewart@my.jcu.edu.au>
2024-11-23 12:52:30 +1000

    Added second test car to verify outputs

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -7,8 +7,14 @@ Silver_service_taxi test
 from silver_service_taxi import silverservicetaxi
 
 hummer = silverservicetaxi("Hummer", 200, 4)
+concept_car = silverservicetaxi("Overpriced car", 100, 2)
 
 print(hummer)
 hummer.start_fare()
 hummer.drive(20)
-print(hummer.get_fare())
+print(f"The fare is: ${hummer.get_fare()}")
+
+print(concept_car)
+concept_car.start_fare()
+concept_car.drive(18)
+print(f"The fare is: ${concept_car.get_fare()}")
