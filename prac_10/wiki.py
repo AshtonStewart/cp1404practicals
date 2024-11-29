9abe3755da1d711fd921b0d03860bc887bbb76d2
AshtonStewart <ashton.stewart@my.jcu.edu.au>
2024-11-29 21:25:15 +1000

    Complete all tasks with error checking

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -11,12 +11,15 @@ def main():
     """Search wikipedia for content relevant to user input"""
     search_prompt = input("Wikipedia content you'd like to search for: ")
     while search_prompt.strip(" ") != "":
-
         try:
-            print(wikipedia.summary(search_prompt))
+            page_details = wikipedia.page(search_prompt)
+            print(f"Webpage title:\n{page_details.title}\n\nUrl:\n{page_details.url}\n\nSummary:\n{page_details.summary}")
         except wikipedia.exceptions.DisambiguationError as e:
-            print("error")
+            print("Disambiguation Error")
+        except wikipedia.exceptions.PageError:
+            print("Page ID doesn't exist")
         search_prompt = input("Wikipedia content you'd like to search for: ")
+    print("Thank you.")
 
 
 main()
